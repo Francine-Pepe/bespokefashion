519cf17a5c087d7cf491e69f36e480d5f37f0300
Jaisree Ramesh <jaisree.ramesh@gmail.com>
2022-01-25 19:18:41 +0100

    Contact us page added

--- fashion-final-project/src/App.js
+++ fashion-final-project/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css"
 import LandingPage from "./Components/LandingPage/LandingPage"
+import Contact from "./Components/Contact/Contact"
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      {/* <LandingPage /> */}
+      <Contact />
     </div>
   )
 }
