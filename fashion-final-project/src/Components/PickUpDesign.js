970410c8f937c6413787e849f066dd29130fc2a5
Francine Melo Pêpe <franmelopepe2@gmail.com>
2022-01-26 00:45:53 +0100

    little fixes to the layout

--- fashion-final-project/src/Components/PickUpDesign.js
+++ fashion-final-project/src/Components/PickUpDesign.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./PickUpDesign.css";
-// import Scissors from "./Images/scissors.png";
-// import Pillow from './Images/sewing_pillow_bg.png';
+
 
 export default function PickUpDesign() {
   return (
     <>
       {/* <div className="outside_container_right"></div> */}
       <div className="main_design_box">
-        {/* <img className="pillow_image" src={Pillow} alt="pillow" style={{width: 150, height: 150}}/> */}
+        <div className="center_detail_scissor"></div>
+        
         <div className="left_container">
           <button className="pick_up_design_button">Our Designs</button>
           <div className="our_design_box"></div>
@@ -18,8 +18,9 @@ export default function PickUpDesign() {
           <button className="pick_up_design_button">Make your Design</button>
           <div className="your_design_box"></div>
         </div>
+        <div className="outside_container_left"></div>
       </div>
-      <div className="outside_container_left"></div>
+      
     </>
   );
 }
