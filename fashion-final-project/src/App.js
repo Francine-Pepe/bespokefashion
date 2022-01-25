49bd7c252ed6d2c88a4050b49f3d40c961604369
Francine Melo Pêpe <franmelopepe2@gmail.com>
2022-01-25 22:49:05 +0100

    pick up design component

--- fashion-final-project/src/App.js
+++ fashion-final-project/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css"
 import LandingPage from "./Components/LandingPage/LandingPage"
+// import PickUpDesign from './Components/PickUpDesign';
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+
+      {/* Take away this part bellow when the router is ready */}
+      {/* <PickUpDesign /> */}
     </div>
   )
 }
