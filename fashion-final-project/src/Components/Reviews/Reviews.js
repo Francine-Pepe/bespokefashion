252a931ef442017b1158cc7fd03aab3af37a364d
kate_lulu <lukoszek.katarzyna@gmail.com>
2022-01-26 22:52:26 +0100

    creating Reviews

---
+++ fashion-final-project/src/Components/Reviews/Reviews.js
@@ -0,0 +1,37 @@
+import React from "react"
+import "./Reviews.css"
+import { useState } from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Modal from "react-bootstrap/Modal"
+import CommentBox from "./CommentBox"
+
+function Reviews() {
+  const [lgShow, setLgShow] = useState(false)
+
+  return (
+    <>
+      <h2 onClick={() => setLgShow(true)}>Reviews</h2>
+
+      <Modal
+        show={lgShow}
+        onHide={() => setLgShow(false)}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header className="Rev-header" closeButton>
+          <Modal.Title
+            id="example-custom-modal-styling-title"
+            className="Rev-title"
+          >
+            <h2>Want to share your experience?</h2>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="Rev-body">
+          <CommentBox />
+        </Modal.Body>
+      </Modal>
+    </>
+  )
+}
+
+export default Reviews
