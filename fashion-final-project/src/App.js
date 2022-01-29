c997d860683376a131ade348f17b159aafc869f6
Jaisree Ramesh <jaisree.ramesh@gmail.com>
2022-01-29 13:33:36 +0100

    measurements page is done

--- fashion-final-project/src/App.js
+++ fashion-final-project/src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
         <Navigation /> 
-            {/* <Outlet /> */}
-            <Measurements />
+            <Outlet />
+            {/* <Measurements /> */}
         <Footer />
     </div>
   )
