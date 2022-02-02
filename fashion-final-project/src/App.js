0f9a6559213e7f79a8c973f90a246975bf523954
kate_lulu <lukoszek.katarzyna@gmail.com>
2022-02-02 21:33:23 +0100

    2

--- fashion-final-project/src/App.js
+++ fashion-final-project/src/App.js
@@ -1,15 +1,15 @@
-//import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import "./App.css"
 import Navigation from "./Components/Navigation/Navigation"
 import Footer from "./Components/Footer/Footer"
-import Measurements from "./Components/Measurements/Measurements"
+//import Measurements from "./Components/Measurements/Measurements"
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/*<Outlet /> */}
-      <Measurements />
+      <Outlet />
+      {/* <Measurements />*/}
       <Footer />
     </div>
   )
