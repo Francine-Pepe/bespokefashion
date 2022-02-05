ede9e7cd7b953209d3266df498a573d61ca0de23
Francine <franmelopepe2@gmail.com>
2022-02-05 18:42:50 +0100

    fixes to makeYourDesignClothes

--- fashion-final-project/src/Components/MakeYourDesignPatterns/MakeYorDesignPatterns.js
+++ fashion-final-project/src/Components/MakeYourDesignPatterns/MakeYorDesignPatterns.js
@@ -12,11 +12,10 @@ export default function MakeYourDesignPatterns() {
         <>
         <div className="make_your_design_color_and_pattern">
             <h3>Make your own Design - here you can choose the desired color or print</h3>
-        
         <div className="main_container_color_and_pattern">
-            <div className="manequin_container">
+            {/* <div className="manequin_container">
                 <img src={Manequin} alt="manequin" />
-            </div>
+            </div> */}
             
             <div className='carousels'>
                 <div className='carousel_container_colors'>
@@ -29,10 +28,7 @@ export default function MakeYourDesignPatterns() {
             </div>
             
             </div>
-
-
         </div>
-        
         <NavLink className="next_page" to="/app/measurement" >Next</NavLink>
         </>
 
