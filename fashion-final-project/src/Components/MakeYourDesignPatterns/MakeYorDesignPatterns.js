4bf4446d396969121b143cf34acce039a0676bfc
Francine <franmelopepe2@gmail.com>
2022-02-07 16:41:47 +0100

    manequim for colors and prints page

--- fashion-final-project/src/Components/MakeYourDesignPatterns/MakeYorDesignPatterns.js
+++ fashion-final-project/src/Components/MakeYourDesignPatterns/MakeYorDesignPatterns.js
@@ -3,7 +3,8 @@ import './MakeYourDesignPatterns.css';
 import ColorsCarousel from '../ColorsCarousel/ColorsCarousel';
 import PrintsCarousel from '../PrintsCarousel/PrintsCarousel.js';
 import { NavLink } from "react-router-dom"
-
+import ManequimTop from '../Manequim/ManequimTop';
+import ManequimBottom from '../Manequim/ManequimBottom';
 
 
 export default function MakeYourDesignPatterns() {
@@ -12,6 +13,12 @@ export default function MakeYourDesignPatterns() {
         <div className="make_your_design_color_and_pattern">
             <h3>Make your own Design - here you can choose the desired color or print</h3>
         <div className="main_container_color_and_pattern">
+            {/* <div className="manequim_container">
+                <ManequimTop />
+                <ManequimBottom />
+
+            </div> */}
+
             {/* <div className="manequin_container">
                 <img src={Manequin} alt="manequin" />
             </div> */}
--- fashion-final-project/src/Components/Manequim/ManequimTop.js
+++ fashion-final-project/src/Components/Manequim/ManequimTop.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import ManequimTopImage from './../Images/manequim_top.png';
-import { useContext } from 'react';
+import React from "react";
+import ManequimTopImage from "./../Images/manequim_top.png";
+import { useContext } from "react";
 import { OrderContext } from "../../orderContext";
+import WhiteBg from "./../Images/white_bg_image.png";
 
 export default function ManequimTop() {
-    const { order } = useContext(OrderContext)
-    return (
-        <>
-        <div>
+  const { order } = useContext(OrderContext);
+  return (
+    <>
+      <div>
         {order.top.url ? (
-          <img src={order.top.url} alt="manequim_bottom" />
+          <div className="wrapper_top_manequim"
+            style={{
+              backgroundImage: `url(${order.fabric.url})`,
+              backgroundRepeat: "repeat-x",
+            }}
+          >
+            <img
+              className="manequim_top_image"
+              src={order.top.url}
+              alt="manequim_bottom"
+            />
+          </div>
         ) : (
-          <img src={ManequimTopImage} alt="manequim_bottom"/>
+          <img src={ManequimTopImage} alt="manequim_bottom" />
         )}
-            {/* <img src={order.top.url} alt='manequim' /> */}
-        </div>
-        </>
-    );
+        {/* <img src={order.top.url} alt='manequim' /> */}
+      </div>
+    </>
+  );
 }
